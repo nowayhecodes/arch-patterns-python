cbd4340ee90e6792d2a2db56e41faf0415aaf8c2
Gustavo Cavalcante <nowayhecodes@gmail.com>
2023-03-08 18:15:18 -0300

    chore: batch model + tests

---
+++ app/models/batch.py
@@ -0,0 +1,77 @@
+from dataclasses import dataclass
+from datetime import date
+from typing import List, NamedTuple, Optional
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    order_id: str
+    sku: str
+    qty: int
+
+
+@dataclass(frozen=True)
+class Name:
+    first_name: str
+    surname: str
+
+
+class Money(NamedTuple):
+    currency: str
+    value: int
+
+
+class Batch:
+    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self._purchased_qty = qty
+        self._allocations = set()
+
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, Batch):
+            return False
+        return __o.reference == self.reference
+
+    def __hash__(self) -> int:
+        return hash(self.reference)
+
+    def __gt__(self, other) -> bool:
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_qty >= line.qty
+
+    @property
+    def allocated_qty(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_qty(self) -> int:
+        return self._purchased_qty - self.allocated_qty
+
+
+class OutOfStock(Exception):
+    pass
+
+
+def allocate(line: OrderLine, batches: List[Batch]) -> str:
+    try:
+        batch = next(b for b in sorted(batches) if b.can_allocate(line))
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f"Out of stock for sku {line.sku}")
